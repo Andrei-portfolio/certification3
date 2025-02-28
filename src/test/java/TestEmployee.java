9ab42d0462bdc51c441952815daef8232a9394b8
Andrei <andrei24845@yandex.ru>
2025-02-28 18:08:41 +0300

    certification

---
+++ src/test/java/TestEmployee.java
@@ -0,0 +1,17 @@
+public class TestEmployee {
+
+    int id = 0;
+    String firstName = "Ivan";
+    String lastName = "Ivanov";
+    String middleName = "Sergei";
+    int companyId;
+    String email = "IIS@Email.com";
+    String url = "fqf.com";
+    String phone = "string";
+    String birthdate = "2024-08-15T12:25:25.165Z";
+    boolean isActive = true;
+
+    TestEmployee(int companyId){
+        this.companyId = companyId;
+    }
+}
